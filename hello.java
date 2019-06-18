6e01ff3be324425dacaccbe2708a4de9763d67ca
tavisca-sverma <sverma@tavisca.in>
2019-06-18 23:44:47 +0530

    Taking number input

--- hello.java
+++ hello.java
@@ -1,8 +1,12 @@
+import java.util.*;
 class hello{
 
 
 public static void main(String args[]){
     System.out.println("Hello");
+    int number;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number to check it's parity>>");
 }
 
 }
