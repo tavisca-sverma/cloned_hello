6bfbe56dff19ad7dcf5051b8022b8a467bebb9ee
tavisca-sverma <sverma@tavisca.in>
2019-06-19 00:18:26 +0530

    Printing the parity of number

--- hello.java
+++ hello.java
@@ -7,7 +7,11 @@ public static void main(String args[]){
     int number;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter a number to check it's parity>>");
-    num=nextInt();
+    number=sc.nextInt();
+    if(number%2==0)
+    System.out.println("The number is Even!");
+    else
+    System.out.println("The number is Odd!");
 }
 
 }
