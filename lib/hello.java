a06acc660c6d219372465f7d4479fe6f91c3e16c
tavisca-sverma <sverma@tavisca.in>
2019-06-19 17:06:06 +0530

    Added greet files and hello file have greet message

--- lib/hello.java
+++ lib/hello.java
@@ -3,9 +3,10 @@ import java.util.*;
 // Author: suraj verma (sverma@tavisca.in)
 class hello{
 
-
+//has greet message at begin
 public static void main(String args[]){
-    System.out.println("Hello");
+    System.out.println("Just to greet You. Have a Good day !");
+     System.out.println("You are doing awesome! ");
     int number;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter a number to check it's parity>>");
