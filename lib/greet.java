471744469f7faee77c8b080933aa12af90084c2b
tavisca-sverma <sverma@tavisca.in>
2019-06-19 16:32:02 +0530

    Added greet files

---
+++ lib/greet.java
@@ -0,0 +1,11 @@
+
+// Author: suraj verma (sverma@tavisca.in)
+class greet{
+
+
+public static void main(String args[]){
+    System.out.println("Just to greet You. Have a Good day !");
+     System.out.println("You are doing awesome! ");
+}
+
+}
